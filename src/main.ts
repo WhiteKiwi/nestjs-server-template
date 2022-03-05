8f177277135d7a4f87b3e0af3df4845b014a6dc1
장지훈 <kiwi.jang@toss.im>
2022-03-05 23:55:26 +0900

    feat(): ValidationPipe, ResponseTransformInterceptor 적용

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Env } from '@config/env';
+import { TransformResponseInterceptor } from '@core/interceptors';
 import { setupSwagger } from '@core/setup';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
@@ -9,6 +11,18 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	setupSwagger(app);
+
+	app.setGlobalPrefix('api', { exclude: ['docs'] });
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+		}),
+	);
+	app.useGlobalInterceptors(new TransformResponseInterceptor());
+	app.enableVersioning({
+		type: VersioningType.URI,
+		defaultVersion: '1',
+	});
 
 	const configService: ConfigService<Env> = app.get(ConfigService);
 	const port = configService.get<number>('PORT') || 3000;
